3c6bbbe767d93b0bf04f1f8352e72a5bf81af3f7
mahesh.takale <mahesh8497>
2022-01-04 18:10:53 +0530

    First commit: Unit test inheritance is a relationship

--- simple-java-app/src/test/java/BmwTests.java
+++ simple-java-app/src/test/java/BmwTests.java
@@ -2,6 +2,7 @@ import clsobj.Bmw;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import clsobj.Car;
 public class BmwTests {
     @DisplayName("Testing Object Nullability")
     @Test
@@ -17,5 +18,22 @@ public class BmwTests {
         int expectedSpeed = 10;
         int actualSpeed = bmw.speed;
         Assertions.assertEquals(expectedSpeed, actualSpeed);
+    }
+    @DisplayName("Test is Bmw is a Car")
+    @Test
+    void testBmwObjectCar() {
+        Bmw bmw = new Bmw();
+        boolean expected = true;
+        boolean actual = bmw instanceof Car;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test Car is Not a BMW")
+    @Test
+    void testCarObjectNotBmw() {
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected, actual);
     }
 }
