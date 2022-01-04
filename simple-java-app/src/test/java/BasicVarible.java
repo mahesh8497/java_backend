62b373c953b606ff8c8ae26d7db370d456fdf5a6
mahesh.takale <mahesh8497>
2022-01-04 12:28:33 +0530

    First commit: Test

---
+++ simple-java-app/src/test/java/BasicVarible.java
@@ -0,0 +1,18 @@
+public class BasicVarible {
+
+        int a=10;
+        char ch='A';
+        boolean b=false;
+        long l=1234567l;
+        short s=11;
+        byte w=12;
+        double d=12.12;
+        float f=1.1f;
+        String ss="s";
+        public void Basic()
+        {
+
+        }
+
+
+    }
