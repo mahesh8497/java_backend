c9a09d5df913384e5234078f7eb8d683f214614b
mahesh.takale <mahesh8497>
2022-01-07 14:04:04 +0530

    design pattern: factory

---
+++ day5/design-pattern/src/main/java/factory/AbcCard.java
@@ -0,0 +1,13 @@
+package factory;
+
+public class AbcCard implements CardProvider {
+
+    public void swipe(float amt) {
+
+    }
+
+
+    public String getBank() {
+        return "abc";
+    }
+}
