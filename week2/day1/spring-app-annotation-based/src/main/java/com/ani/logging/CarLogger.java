9cdd9179befaad0a7cc64a1392ab1f39db45a28b
mahesh.takale <mahesh8497>
2022-01-10 15:01:01 +0530

    Spring core : spring point cuts

--- week2/day1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
+++ week2/day1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CarLogger {
-
+    @Pointcut("execution(public * com.ani.car.Car.speedUp(..))")
+    public void pointCutAfterExecution() { }
     @Before("execution(* com.ani.car.*.*(..))")
     public void logBeforeAnyMethodExecutionCarPackage(JoinPoint jp) {
         System.out.println("Before Execution I am getting printed");
         System.out.println("Executing method "+jp.getSignature());
         System.out.println("After this line you will see method output");
         System.out.println("----------------");
+    }
+    @After("pointCutAfterExecution()")
+    public void logAfterAnyMethods(JoinPoint jp) {
+        System.out.println("After Execution I am getting printed");
     }
 }
