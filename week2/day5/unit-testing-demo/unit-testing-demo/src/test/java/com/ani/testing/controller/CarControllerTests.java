01fe667f1f78ab81eadd7748bba7fb57a5a024af
mahesh.takale <mahesh8497>
2022-01-14 11:59:10 +0530

    unit testing Service  layer

--- week2/day5/unit-testing-demo/unit-testing-demo/src/test/java/com/ani/testing/controller/CarControllerTests.java
+++ week2/day5/unit-testing-demo/unit-testing-demo/src/test/java/com/ani/testing/controller/CarControllerTests.java
@@ -2,6 +2,7 @@ package com.ani.testing.controller;
 
 import com.ani.testing.domain.Car;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,31 +11,24 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.sql.Date;
 import java.time.LocalDate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CarControllerTests {
 
-public class CarControllerTest {
     @Autowired
-    private TestRestTemplate template;
+    private TestRestTemplate template; // it is wrapper over http client
 
     @LocalServerPort
     private int port;
 
-    @DisplayName("GET -Car-checking object is non null")
+    @DisplayName("GET - Car - Checking Object is non null")
     @Test
-    public void testGetMethod(){
-//        var builder=new StringBuilder();
-//        builder.append("http://")
-//                .append("localhost")
-//                .append(":")
-//                .append("/car");
-//        Car car = template.getForObject(
-//                "http://localhost:8080/car",
-//                Car.class
-//        );
+    public void testGetMethod() {
+
         String url = "http://" + "localhost" +":" + port +"/car";
 
         Car car = template.getForObject( // GET method
@@ -44,17 +38,27 @@ public class CarControllerTest {
 
         Assertions.assertNotNull(car);
     }
+
+    @DisplayName("GET - Car - Checking object content")
     @Test
-    public void testGetStatusCode(){
-        String url="http://"+"localhost"+":"+port+"/car";
-        ResponseEntity<Car>entity=template.getForEntity(url,Car.class);
+    public void testObjectContent() {
+        String url = "http://" + "localhost" +":" + port +"/car";
+        var re = template.getForEntity(url, Car.class);
+        var car = re.getBody();
+        Assertions.assertEquals(car.getId(), 10 );
+    }
+
+    @DisplayName("GET - Car - Checking Status Code")
+    @Test
+    public void testGetStatusCode() {
+        String url = "http://" + "localhost" +":" + port +"/car";
+        ResponseEntity<Car> entity = template.getForEntity(url, Car.class);
         Assertions.assertEquals(
                 HttpStatus.OK,
                 entity.getStatusCode()
-
         );
-
     }
+
     @DisplayName("POST - Car - Saving Car Object")
     @Test
     public void testPostMethod() {
@@ -67,9 +71,8 @@ public class CarControllerTest {
         var re = template.postForEntity(
                 url,
                 car,
-                Car.class
+                Car.class // type information of Car -> this is skeleton of the car
         );
         Assertions.assertEquals(HttpStatus.CREATED, re.getStatusCode());
     }
-
 }
